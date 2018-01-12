2a59bf9ec6f50227e22f97601d984db6a91935bc
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-01-12 12:06:32 -0500

    Added initial project boilerplate.

---
+++ android/src/edu/gvsu/cis/spacejourney/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package edu.gvsu.cis.spacejourney;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import edu.gvsu.cis.spacejourney.SpaceJourney;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceJourney(), config);
+	}
+}
---
+++ desktop/src/edu/gvsu/cis/spacejourney/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.gvsu.cis.spacejourney.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.gvsu.cis.spacejourney.SpaceJourney;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceJourney(), config);
+	}
+}
