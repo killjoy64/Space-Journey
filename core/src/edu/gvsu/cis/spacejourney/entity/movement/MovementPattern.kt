7df8e690df67f2730af611f29919cf174fa7ab02
John Doneth <doneth7@gmail.com>
2018-02-07 14:06:26 -0500

    Yes git... These files too

--- core/src/edu/gvsu/cis/spacejourney/entity/movement/MovementPattern.kt
+++ core/src/edu/gvsu/cis/spacejourney/entity/movement/MovementPattern.kt
@@ -5,6 +5,6 @@ import com.badlogic.gdx.math.Vector2
 abstract class MovementPattern {
 
     // Returns a normalized 0..1 x, 0..1 y vector
-    abstract fun getMovement(position : Vector2) : Vector2
+    abstract fun getMovement(position: Vector2): Vector2
 
 }
--- core/src/edu/gvsu/cis/spacejourney/input/GameContactListener.java
+++ core/src/edu/gvsu/cis/spacejourney/input/GameContactListener.java
@@ -17,60 +17,60 @@ import edu.gvsu.cis.spacejourney.managers.GameDataManager;
  */
 public class GameContactListener implements ContactListener {
 
-  private GameDataManager gameData;
+    private GameDataManager gameData;
 
-  public GameContactListener() {
-    this.gameData = GameDataManager.getInstance();
-  }
-
-  @Override
-  public void beginContact(Contact contact) {
-    Fixture a = contact.getFixtureA();
-    Fixture b = contact.getFixtureB();
-    Object entityA = a.getBody().getUserData();
-    Object entityB = b.getBody().getUserData();
-    int currentLives = gameData.getLives();
-
-    if (entityB instanceof EvilSpaceship && entityA instanceof PlayerSpaceship) {
-      PlayerSpaceship player = (PlayerSpaceship) entityA;
-      player.takeDamage();
-      gameData.setLives(currentLives - 1);
-    } else if (entityA instanceof EvilSpaceship && entityB instanceof PlayerSpaceship) {
-      PlayerSpaceship player = (PlayerSpaceship) entityB;
-      player.takeDamage();
-      gameData.setLives(currentLives - 1);
+    public GameContactListener() {
+        this.gameData = GameDataManager.getInstance();
     }
 
-    if (entityA instanceof Collectible && entityB instanceof PlayerSpaceship) {
-      Collectible c = (Collectible) entityA;
-      c.collect();
-    } else if (entityA instanceof PlayerSpaceship && entityB instanceof Collectible) {
-      Collectible c = (Collectible) entityB;
-      c.collect();
+    @Override
+    public void beginContact(Contact contact) {
+        Fixture a = contact.getFixtureA();
+        Fixture b = contact.getFixtureB();
+        Object entityA = a.getBody().getUserData();
+        Object entityB = b.getBody().getUserData();
+        int currentLives = gameData.getLives();
+
+        if (entityB instanceof EvilSpaceship && entityA instanceof PlayerSpaceship) {
+            PlayerSpaceship player = (PlayerSpaceship) entityA;
+            player.takeDamage();
+            gameData.setLives(currentLives - 1);
+        } else if (entityA instanceof EvilSpaceship && entityB instanceof PlayerSpaceship) {
+            PlayerSpaceship player = (PlayerSpaceship) entityB;
+            player.takeDamage();
+            gameData.setLives(currentLives - 1);
+        }
+
+        if (entityA instanceof Collectible && entityB instanceof PlayerSpaceship) {
+            Collectible c = (Collectible) entityA;
+            c.collect();
+        } else if (entityA instanceof PlayerSpaceship && entityB instanceof Collectible) {
+            Collectible c = (Collectible) entityB;
+            c.collect();
+        }
+
+        if (entityA instanceof Laser && entityB instanceof Enemy) {
+            Enemy e = (Enemy) entityB;
+            e.takeDamage();
+        } else if (entityB instanceof Laser && entityA instanceof Enemy) {
+            Enemy e = (Enemy) entityA;
+            e.takeDamage();
+        }
+
     }
 
-    if (entityA instanceof Laser && entityB instanceof Enemy) {
-      Enemy e = (Enemy) entityB;
-      e.takeDamage();
-    } else if (entityB instanceof Laser && entityA instanceof Enemy) {
-      Enemy e = (Enemy) entityA;
-      e.takeDamage();
+    @Override
+    public void endContact(Contact contact) {
+
     }
 
-  }
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
 
-  @Override
-  public void endContact(Contact contact) {
+    }
 
-  }
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
 
-  @Override
-  public void preSolve(Contact contact, Manifold oldManifold) {
-
-  }
-
-  @Override
-  public void postSolve(Contact contact, ContactImpulse impulse) {
-
-  }
+    }
 }
