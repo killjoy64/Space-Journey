3d410ab571f17cdb9e53e24b1721760edf30be08
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-02-09 10:52:44 -0500

    LevelSelectScreen

---
+++ core/src/edu/gvsu/cis/spacejourney/screens/LevelSelectScreen.kt
@@ -0,0 +1,23 @@
+package edu.gvsu.cis.spacejourney.screens
+
+import edu.gvsu.cis.spacejourney.SpaceJourney
+
+class LevelSelectScreen(game: SpaceJourney) : BaseScreen(game, "LevelSelectScreen") {
+
+  override fun show() {
+    super.show()
+  }
+
+  override fun render(delta: Float) {
+    super.render(delta)
+  }
+
+  override fun resize(width: Int, height: Int) {
+    super.resize(width, height)
+  }
+
+  override fun dispose() {
+    super.dispose()
+  }
+
+}
