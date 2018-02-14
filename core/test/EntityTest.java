8e70ea51d40e42718d8829acbc2f1192b0ec5764
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-02-14 10:41:18 -0500

    Working on testing...

--- core/test/EntityTest.java
+++ core/test/EntityTest.java
@@ -1,32 +1,81 @@
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import edu.gvsu.cis.spacejourney.Constants;
+import edu.gvsu.cis.spacejourney.SpaceJourney;
 import edu.gvsu.cis.spacejourney.entity.Entity;
 import edu.gvsu.cis.spacejourney.entity.PlayerSpaceship;
-import org.junit.BeforeClass;
+import edu.gvsu.cis.spacejourney.entity.enemy.Enemy;
+import edu.gvsu.cis.spacejourney.entity.enemy.EvilSpaceship;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.File;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EntityTest extends GameTest {
 
-    @Test
-    public void testEntityCreate() {
-        Gdx.app.postRunnable(new Runnable() {
-            @Override
-            public void run() {
-                Camera camera = new OrthographicCamera();
-                FitViewport viewport = new FitViewport(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
-                Stage stage = new Stage(viewport);
-                Entity e = new PlayerSpaceship(stage);
-                stage.addActor(e);
-                int actors = stage.getActors().size;
-                assertEquals(actors, 1);
-            }
-        });
-    }
+  private Camera camera;
+  private FitViewport viewport;
+  private Stage stage;
+  private World world;
+
+  /**
+   * Test to make sure an entity can be created and added to a stage,
+   * as well as a world. The first test can only execute once
+   * Gdx is ready.
+   */
+  @Test
+  public void testPlayerCreate() {
+    Gdx.app.postRunnable(() -> {
+      camera = new OrthographicCamera();
+      viewport = new FitViewport(
+          Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
+      stage = new Stage(viewport);
+      world = new World(new Vector2(0.0f, 0.0f), false);
+      Entity e = new PlayerSpaceship(stage);
+      stage.addActor(e);
+      int actors = stage.getActors().size;
+      int bodies = world.getBodyCount();
+      assertEquals("Stage actor count increases.", actors, 1);
+
+      e.createBody(world);
+      assertEquals("World body count increases.", world.getBodyCount(), bodies + 1);
+
+      world.destroyBody(e.getBody());
+      assertEquals("World deletes body successfully.", world.getBodyCount(), bodies);
+    });
+  }
+
+  @Test(expected = GdxRuntimeException.class)
+  public void testEnemyCreateAssetNotLoaded() {
+    Enemy e = new EvilSpaceship(stage);
+  }
+
+  @Test
+  public void testEnemyCreate() {
+    getAssets().load("spaceship3.png", Texture.class);
+    getAssets().finishLoading();
+    Enemy e = new EvilSpaceship(stage);
+    stage.addActor(e);
+    int actors = stage.getActors().size;
+    int bodies = world.getBodyCount();
+    assertEquals("Stage actor count increases.", actors, 1);
+
+    e.createBody(world);
+    assertEquals("World body count increases.", world.getBodyCount(), bodies + 1);
+
+    world.destroyBody(e.getBody());
+    assertEquals("World deletes body successfully.", world.getBodyCount(), bodies);
+  }
 
 }
--- core/test/GameTest.java
+++ core/test/GameTest.java
@@ -1,6 +1,7 @@
 import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -9,31 +10,54 @@ import org.junit.BeforeClass;
 import org.mockito.Mockito;
 
 public class GameTest {
-    // This is our "test" application
-    private static Application application;
+  // This is our "test" application
+  private static Application application;
 
-    // Before running any tests, initialize the application with the headless backend
-    @BeforeClass
-    public static void init() {
-        // Note that we don't need to implement any of the listener's methods
-        application = new HeadlessApplication(new ApplicationListener() {
-            @Override public void create() {}
-            @Override public void resize(int width, int height) {}
-            @Override public void render() {}
-            @Override public void pause() {}
-            @Override public void resume() {}
-            @Override public void dispose() {}
-        });
-        // Use Mockito to mock the OpenGL methods since we are running headlessly
-        Gdx.gl20 = Mockito.mock(GL20.class);
-        Gdx.gl = Gdx.gl20;
-    }
+  private static AssetManager assets;
 
-    // After we are done, clean up the application
-    @AfterClass
-    public static void cleanUp() {
-        // Exit the application first
-        application.exit();
-        application = null;
-    }
+  // Before running any tests, initialize the application with the headless backend
+  @BeforeClass
+  public static void init() {
+    // Note that we don't need to implement any of the listener's methods
+    application = new HeadlessApplication(new ApplicationListener() {
+      @Override public void create() {
+        assets = new AssetManager();
+      }
+
+      @Override public void resize(int width, int height) {
+
+      }
+
+      @Override public void render() {
+
+      }
+
+      @Override public void pause() {
+
+      }
+
+      @Override public void resume() {
+
+      }
+
+      @Override public void dispose() {
+
+      }
+    });
+    // Use Mockito to mock the OpenGL methods since we are running headlessly
+    Gdx.gl20 = Mockito.mock(GL20.class);
+    Gdx.gl = Gdx.gl20;
+  }
+
+  public static AssetManager getAssets() {
+    return assets;
+  }
+
+  // After we are done, clean up the application
+  @AfterClass
+  public static void cleanUp() {
+    // Exit the application first
+    application.exit();
+    application = null;
+  }
 }
