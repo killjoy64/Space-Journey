0b2cbdbd5a9c56359318a42e732fb7c169bbb763
Kyle Flynn <kyle.flynn5798@yahoo.com>
2018-02-14 13:41:52 -0500

    Reworked JUnit testing suite.

--- core/test/EntityTest.java
+++ core/test/EntityTest.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.graphics.Camera;
@@ -10,82 +13,54 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import edu.gvsu.cis.spacejourney.Constants;
-import edu.gvsu.cis.spacejourney.SpaceJourney;
-import edu.gvsu.cis.spacejourney.entity.Entity;
 import edu.gvsu.cis.spacejourney.entity.PlayerSpaceship;
 import edu.gvsu.cis.spacejourney.entity.enemy.Enemy;
 import edu.gvsu.cis.spacejourney.entity.enemy.EvilSpaceship;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 
-import java.io.File;
-import java.util.concurrent.CountDownLatch;
+@RunWith(GameTest.class)
+public class EntityTest {
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+  @Mock
+  private static Camera camera;
 
-public class EntityTest extends GameTest {
+  @Mock
+  private static FitViewport viewport;
 
-  private Camera camera;
-  private FitViewport viewport;
-  private Stage stage;
-  private World world;
+  @Mock
+  private static Stage stage;
 
-  /**
-   * Test to make sure an entity can be created and added to a stage,
-   * as well as a world. The first test can only execute once
-   * Gdx is ready.
-   */
-  @Test
-  public void testPlayerCreate() {
-    Gdx.app.postRunnable(() -> {
-      camera = new OrthographicCamera();
-      viewport = new FitViewport(
-          Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
-      stage = new Stage(viewport);
-      world = new World(new Vector2(0.0f, 0.0f), false);
-      Entity e = new PlayerSpaceship(stage);
-      stage.addActor(e);
-      int actors = stage.getActors().size;
-      int bodies = world.getBodyCount();
-      assertEquals("Stage actor count increases.", actors, 1);
+  @Mock
+  private static World world;
 
-      e.createBody(world);
-      assertEquals("World body count increases.", world.getBodyCount(), bodies + 1);
-
-      world.destroyBody(e.getBody());
-      assertEquals("World deletes body successfully.", world.getBodyCount(), bodies);
-    });
+  @BeforeClass
+  public static void setup() {
+    camera = new OrthographicCamera();
+    viewport = new FitViewport(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
+    stage = mock(Stage.class);
+    world = new World(new Vector2(0.0f, 0.0f), false);
   }
 
   /**
-   * Test that should throw an error. First, the stage
-   * is not successfully loaded, and second the
-   * asset should not be loaded yet.
+   * Test that should fail because the player's asset image
+   * was never properly loaded.
+   */
+  @Test(expected = GdxRuntimeException.class)
+  public void testPlayerCreateAssetNotLoaded() {
+    PlayerSpaceship player = new PlayerSpaceship(stage);
+  }
+
+  /**
+   * Test that should fail because the enemy's asset image
+   * was never properly loaded.
    */
   @Test(expected = GdxRuntimeException.class)
   public void testEnemyCreateAssetNotLoaded() {
-    Enemy e = new EvilSpaceship(stage);
-  }
-
-  @Test
-  public void testEnemyCreate() {
-    Gdx.app.postRunnable(() -> {
-      getAssets().load("spaceship3.png", Texture.class);
-      getAssets().finishLoading();
-      TextureRegion region = new TextureRegion(getAssets().get("spaceship3.png", Texture.class));
-      Enemy e = new EvilSpaceship(stage, region);
-      stage.addActor(e);
-      int actors = stage.getActors().size;
-      int bodies = world.getBodyCount();
-      assertEquals("Stage actor count increases.", actors, 1);
-
-      e.createBody(world);
-      assertEquals("World body count increases.", world.getBodyCount(), bodies + 1);
-
-      world.destroyBody(e.getBody());
-      assertEquals("World deletes body successfully.", world.getBodyCount(), bodies);
-    });
+    Enemy enemy = new EvilSpaceship(stage);
   }
 
 }
--- core/test/GameTest.java
+++ core/test/GameTest.java
@@ -1,63 +1,84 @@
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.mockito.Mockito;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.InitializationError;
 
-public class GameTest {
-  // This is our "test" application
-  private static Application application;
+import static org.mockito.Mockito.mock;
 
-  private static AssetManager assets;
+public class GameTest extends BlockJUnit4ClassRunner implements ApplicationListener {
 
-  // Before running any tests, initialize the application with the headless backend
-  @BeforeClass
-  public static void init() {
-    // Note that we don't need to implement any of the listener's methods
-    application = new HeadlessApplication(new ApplicationListener() {
-      @Override public void create() {
-        assets = new AssetManager();
-      }
+  private Map<FrameworkMethod, RunNotifier> invokeInRender = new HashMap<FrameworkMethod, RunNotifier>();
 
-      @Override public void resize(int width, int height) {
+  public GameTest(Class<?> klass) throws InitializationError {
+    super(klass);
+    HeadlessApplicationConfiguration conf = new HeadlessApplicationConfiguration();
 
-      }
-
-      @Override public void render() {
-
-      }
-
-      @Override public void pause() {
-
-      }
-
-      @Override public void resume() {
-
-      }
-
-      @Override public void dispose() {
-
-      }
-    });
-    // Use Mockito to mock the OpenGL methods since we are running headlessly
-    Gdx.gl20 = Mockito.mock(GL20.class);
-    Gdx.gl = Gdx.gl20;
+    new HeadlessApplication(this, conf);
+    Gdx.gl = mock(GL20.class);
   }
 
-  public static AssetManager getAssets() {
-    return assets;
+  @Override
+  public void create() {
   }
 
-  // After we are done, clean up the application
-  @AfterClass
-  public static void cleanUp() {
-    // Exit the application first
-    application.exit();
-    application = null;
+  @Override
+  public void resume() {
   }
+
+  @Override
+  public void render() {
+    synchronized (invokeInRender) {
+      for (Map.Entry<FrameworkMethod, RunNotifier> each : invokeInRender.entrySet()) {
+        super.runChild(each.getKey(), each.getValue());
+      }
+      invokeInRender.clear();
+    }
+  }
+
+  @Override
+  public void resize(int width, int height) {
+  }
+
+  @Override
+  public void pause() {
+  }
+
+  @Override
+  public void dispose() {
+  }
+
+  @Override
+  protected void runChild(FrameworkMethod method, RunNotifier notifier) {
+    synchronized (invokeInRender) {
+      // add for invoking in render phase, where gl context is available
+      invokeInRender.put(method, notifier);
+    }
+    // wait until that test was invoked
+    waitUntilInvokedInRenderMethod();
+  }
+
+  /**
+   *
+   */
+  private void waitUntilInvokedInRenderMethod() {
+    try {
+      while (true) {
+        Thread.sleep(10);
+        synchronized (invokeInRender) {
+          if (invokeInRender.isEmpty())
+            break;
+        }
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
 }
