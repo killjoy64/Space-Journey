daef20a4dfd7b53ee982259a7cbd5670312967fa
John Doneth <Doneth7@gmail.com>
2018-02-26 19:59:32 -0500

    Added TestEnemyMovement Test

--- core/test/EnemyTest.java
+++ core/test/EnemyTest.java
@@ -11,12 +11,16 @@ import edu.gvsu.cis.spacejourney.SpaceJourney;
 import edu.gvsu.cis.spacejourney.entity.Graveyard;
 import edu.gvsu.cis.spacejourney.entity.enemy.Enemy;
 import edu.gvsu.cis.spacejourney.entity.enemy.EvilSpaceship;
+import edu.gvsu.cis.spacejourney.entity.movement.LinearMovement;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -27,16 +31,14 @@ import static org.mockito.Mockito.when;
 @RunWith(GameTest.class)
 public class EnemyTest {
 
-  private static Camera camera;
-  private static FitViewport viewport;
   private static Stage stage;
   private static World world;
   private static Enemy enemy;
 
   @BeforeClass
   public static void setup() {
-    camera = new OrthographicCamera();
-    viewport = new FitViewport(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
+    Camera camera = new OrthographicCamera();
+    FitViewport viewport = new FitViewport(Constants.VIRTUAL_WIDTH, Constants.VIRTUAL_HEIGHT, camera);
     stage = mock(Stage.class);
     world = new World(new Vector2(0.0f, 0.0f), false);
 
@@ -81,9 +83,20 @@ public class EnemyTest {
     assertTrue(Graveyard.BODIES.contains(enemy.getBody()));
   }
 
+
+  /*
+   * Test the enemy objects movement by seeing how its Box2D body reacts to a LinearMovement pattern
+   */
   @Test
   public void testEnemyMovement() {
-    // TODO - John help please.
+    enemy.setMovementPattern(new LinearMovement(new Vector2(1.0f, 1.0f)));
+
+    enemy.act(1.0f);
+
+    world.step(1.0f / 60.0f, 6, 2);
+
+    assertEquals(enemy.getBody().getPosition().x, 0.336f, 0.01f);
+    assertEquals(enemy.getBody().getPosition().y, 0.336f, 0.01f);
   }
 
   @After
